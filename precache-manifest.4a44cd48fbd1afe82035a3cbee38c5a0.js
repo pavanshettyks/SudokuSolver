b839831016a169f0233223a0eb441a5d738ff876
pavanshettyks <pavanksshetty@gmail.com>
2020-02-04 01:00:30 -0800

    Updates

---
+++ precache-manifest.4a44cd48fbd1afe82035a3cbee38c5a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ebb50dc49dbf19f0a2c0349ee65ff5",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "ebc0a1d1808a556db292",
+    "url": "/sudoku/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2776ec61354269bd0198",
+    "url": "/sudoku/static/js/2.f6f77da3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/sudoku/static/js/2.f6f77da3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebc0a1d1808a556db292",
+    "url": "/sudoku/static/js/main.0c8e109f.chunk.js"
+  },
+  {
+    "revision": "80fdc32de6d895578ae2",
+    "url": "/sudoku/static/js/runtime-main.c8e770ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sudoku/static/media/logo.5d5d9eef.svg"
+  }
+]);
